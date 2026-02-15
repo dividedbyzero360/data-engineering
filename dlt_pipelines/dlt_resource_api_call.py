b8e4cf9d4d1a54ba5e8070af07bc71c88476ab9c
dividedbyzero360 <wastabir.alam@gmail.com>
2026-02-14 20:44:56 -0500

    Add new dlt_resource_api_call module for GitHub API interaction and update VSCode launch configuration for debugging

---
+++ dlt_pipelines/dlt_resource_api_call.py
@@ -0,0 +1,26 @@
+import dlt
+from dlt.sources.helpers.rest_client import RESTClient
+from dlt.sources.helpers.rest_client.auth import BearerTokenAuth
+from dlt.sources.helpers.rest_client.paginators import HeaderLinkPaginator
+import os
+
+API_TOKEN = os.getenv('GITHUB_TOKEN')
+BASE_URL="https://api.github.com/repos/DataTalksClub/data-engineering-zoomcamp"
+
+# @dlt.resource()
+def paginated_getter():
+    client = RESTClient(
+        base_url = BASE_URL,
+        auth = BearerTokenAuth(token=API_TOKEN),
+        paginator=HeaderLinkPaginator(links_next_key="next")
+    )
+
+    page_number = 1
+    for page in client.paginate("events"):
+        if page_number==2:
+            break
+        page_number+= 1
+        yield page
+
+for record in paginated_getter():
+    print(record)
